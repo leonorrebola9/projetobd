281b211e22ab61a7c40000789e2c7c41b0b22373
leonorrebola9 <leonor.rebola@ubi.pt>
2025-12-13 18:11:56 +0000

    organização no SQL

---
+++ Documentos SQL/Alertas/trigger_Alert.sql
@@ -0,0 +1,68 @@
+CREATE OR ALTER TRIGGER trg_AutoAlert
+ON Orbital_Parameter
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO Alert (
+        Asteroid_ID, 
+        Orbital_ID,
+        dap,        -- Data Aproximação
+        p,          -- Probabilidade
+        torino,     -- Nível (0-4)
+        status,     -- Cor
+        Priority,   -- Prioridade
+        Description,
+        Alert_Date
+    )
+    SELECT 
+        i.Asteroid_ID,
+        NULL, -- Se tiveres coluna Orbital_ID na tabela Orbital_Parameter, troca 'NULL' por 'i.Orbital_ID'
+        
+        -- Estimativa simples da data (Hoje + Dias do MOID)
+        DATEADD(day, i.moid_ld, GETDATE()), 
+        NULL, -- p (Probabilidade)
+
+        -- Classificação Torino
+        CASE 
+            WHEN A.diameter > 0.03 AND i.moid_ld < 1 THEN 4
+            WHEN A.diameter > 0.05 AND i.moid_ld < 5 AND i.rms < 0.3 THEN 3
+            WHEN A.diameter > 0.1 AND i.moid_ld BETWEEN 5 AND 20 THEN 2
+            WHEN A.pha = 'Y' AND A.diameter BETWEEN 0.05 AND 0.5 AND i.moid_ld BETWEEN 20 AND 100 THEN 1
+            ELSE 0 
+        END,
+
+        -- Cor
+        CASE 
+            WHEN A.diameter > 0.03 AND i.moid_ld < 1 THEN 'Vermelho'
+            WHEN A.diameter > 0.05 AND i.moid_ld < 5 AND i.rms < 0.3 THEN 'Laranja'
+            WHEN A.diameter > 0.1 AND i.moid_ld BETWEEN 5 AND 20 THEN 'Amarelo'
+            WHEN A.pha = 'Y' AND A.diameter BETWEEN 0.05 AND 0.5 AND i.moid_ld BETWEEN 20 AND 100 THEN 'Verde'
+            ELSE 'Sem Alerta'
+        END,
+
+        -- Prioridade
+        CASE 
+            WHEN (i.moid_ld < 1 AND A.diameter > 0.01) 
+              OR (A.pha = 'Y' AND A.diameter > 0.1 AND i.rms > 0.8 AND i.moid_ld < 20) THEN 'Alta'
+            WHEN (A.diameter > 0.5 AND i.moid_ld < 50)
+              OR (i.e > 0.05 OR i.i > 2) THEN 'Media'
+            WHEN (A.albedo > 0.3 OR i.e > 0.8 OR i.i > 70) AND A.diameter > 0.2 THEN 'Baixa'
+            ELSE 'Normal'
+        END,
+
+        'Alerta Auto (MOID: ' + CAST(CAST(i.moid_ld AS DECIMAL(10,2)) AS VARCHAR) + ')',
+        GETDATE()
+
+    FROM inserted i
+    INNER JOIN Asteroid A ON i.Asteroid_ID = A.Asteroid_ID
+    -- Filtro: Só gera alerta se houver alguma condição relevante
+    WHERE 
+        (A.diameter > 0.03 AND i.moid_ld < 1) OR 
+        (A.diameter > 0.05 AND i.moid_ld < 5 AND i.rms < 0.3) OR 
+        (A.diameter > 0.1 AND i.moid_ld BETWEEN 5 AND 20) OR 
+        (A.pha = 'Y') OR
+        (A.albedo > 0.3 OR i.e > 0.8 OR i.i > 70);
+END
+GO
---
+++ Documentos SQL/Tabelas gerais/tabela_intersecao.sql
@@ -0,0 +1,50 @@
+USE projeto;
+GO
+
+-- Criar a tabela dbo.dados e eliminar caso exista
+IF OBJECT_ID('dbo.dados', 'U') IS NOT NULL 
+    DROP TABLE dbo.dados;
+GO
+
+-- Colunas a usar para cada tabela
+SELECT 
+    -- Colunas proveniente de dbo.neo
+    N.h,
+    N.albedo,
+    CAST(N.name AS VARCHAR(200)) AS Name_Neo, 
+    N.diameter,
+    N.diameter_sigma,
+    N.pha,
+    N.neo,
+    N.epoch_cal,
+    N.sigma_i,
+    N.i,
+    N.epoch,
+    N.e,
+    N.sigma_e,
+    N.moid_ld,
+    N.rms,
+    N.a,
+    N.sigma_a,
+    N.q,
+    N.sigma_q,
+    N.tp_cal,
+    N.sigma_tp,
+
+    -- Colunas proveniente de dbo.MPCORB
+    M.Arc,
+    M.Opp,
+    M.Computer,
+    M.Obs,
+    M.M AS Mean_Anomaly_MPCORB, 
+    
+    -- Nome MPCORB
+    LTRIM(RTRIM(CAST(M.Nome AS VARCHAR(MAX)))) AS Name_MPCORB
+
+-- Inserção dos dados para dbo.dados
+INTO dbo.dados 
+FROM dbo.neo AS N
+INNER JOIN dbo.MPCORB AS M
+    ON REPLACE(REPLACE(REPLACE(CAST(N.name AS VARCHAR(MAX)), ' ', ''), '(', ''), ')', '') 
+       = 
+       REPLACE(REPLACE(REPLACE(CAST(M.Nome AS VARCHAR(MAX)), ' ', ''), '(', ''), ')', '');
